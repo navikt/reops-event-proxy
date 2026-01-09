b9868ead1309dd852c4c998b29e8aca0878d5baa
Kenneth Isaksen <kenneth.bakke.isaksen@nav.no>
2026-01-09 12:35:28 +0100

    feat: add kafka metric

--- src/main/kotlin/no/nav/reops/event/EventPublishService.kt
+++ src/main/kotlin/no/nav/reops/event/EventPublishService.kt
@@ -1,5 +1,7 @@
 package no.nav.reops.event
 
+import io.micrometer.core.instrument.Counter
+import io.micrometer.core.instrument.MeterRegistry
 import org.apache.kafka.clients.producer.ProducerRecord
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
@@ -15,11 +17,14 @@ const val USER_AGENT = "User-Agent"
 @Service
 class EventPublishService(
     private val kafkaTemplate: KafkaTemplate<String, Event>,
+    meterRegistry: MeterRegistry,
     @Value("\${spring.kafka.topic}") private val topic: String
 ) {
     private val logger = LoggerFactory.getLogger(EventPublishService::class.java)
+    private val kafkaEventCounter: Counter = meterRegistry.counter("kafka_events_created_total", "topic", topic)
 
     fun publishEventAsync(event: Event, userAgent: String): CompletableFuture<SendResult<String, Event>> {
+        kafkaEventCounter.increment()
         val key = UUID.randomUUID().toString()
         val record = ProducerRecord(topic, key, event).apply {
             headers().add(USER_AGENT, userAgent.toByteArray(StandardCharsets.UTF_8))
